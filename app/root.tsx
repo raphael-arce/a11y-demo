725263d218369ce67eca85c43fd5e4d82811a121
Raphael Arce <raphael.arce@ts.berlin>
2025-05-12 07:30:39 +0200

    feat: use lang de
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import type { Route } from "./+types/root";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="de">
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
